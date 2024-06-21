7dd5ee1d4db131e83ff3ba53df2f10c827f7cab3
Ikanga93 <153716887+Ikanga93@users.noreply.github.com>
2024-06-20 21:14:52 -0400

    commit

--- us_debt_by_president_dag.py
+++ us_debt_by_president_dag.py
@@ -108,6 +108,34 @@ def transform_task(data):
         # Remove the commas from the debt_outstanding_amt and convert it to float
         data['debt_outstanding_amt'] = data['debt_outstanding_amt'].astype(float)
 
+        # I thought it will be great if I create more columns to make the data more informative
+        # Create a new column to show the debt in trillions
+        # data['debt_outstanding_trillions'] = data['debt_outstanding_amt'] / 1e12
+        # Create a new column to show the debt in billions
+        # data['debt_outstanding_billions'] = data['debt_outstanding_amt'] / 1e9
+        # Create a new column to show the debt in millions
+        # data['debt_outstanding_millions'] = data['debt_outstanding_amt'] / 1e6
+        # Round the debt_outstanding_trillions and debt_outstanding_billions columns to 2 decimal places
+        # data['debt_outstanding_trillions'] = data['debt_outstanding_trillions'].round(2)
+        # data['debt_outstanding_billions'] = data['debt_outstanding_billions'].round(2)
+        # data['debt_outstanding_millions'] = data['debt_outstanding_millions'].round(2)
+        # Calculate the debt per president
+        # data['debt_per_president'] = data.groupby('president')['debt_outstanding_trillions'].diff().fillna(0)
+        # Calculate the debt increase per year
+        # data['debt_increase_per_year'] = data.groupby('president')['debt_outstanding_trillions'].diff().fillna(0) / 4
+        # Calculate the debt increase per day
+        # data['debt_increase_per_day'] = data.groupby('president')['debt_outstanding_trillions'].diff().fillna(0) / 365
+        # Calculate the debt increase per hour
+        # data['debt_increase_per_hour'] = data.groupby('president')['debt_outstanding_trillions'].diff().fillna(0) / 8760
+        # Need to sort the data by record_fiscal_year in ascending order
+        # data = data.sort_values('record_fiscal_year', ascending=True)
+        # Calculate how much each president added to the debt without repeating the president's name
+        # data['debt_added'] = data.groupby('president')['debt_outstanding_trillions'].diff().fillna(0)
+        # Geting the total debt added by each president
+        # data['total_debt_added'] = data.groupby('president')['debt_added'].cumsum()
+        # 
+    
+
         logging.info('Data transformed successfully')
         return data
     except Exception as e:
